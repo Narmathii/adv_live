72a7cd0d156691a9fcb95798eabce0801240b500
Narmathi G <karthik@appteq.in>
2025-09-22 18:17:57 +0530

    Fix : Payment

--- app/Views/payment.php
+++ app/Views/payment.php
@@ -27,69 +27,100 @@ require("components/head.php");
     <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
 
     <script>
+
         var options = {
-            "key": "<?= esc($key_id) ?>",
-            "amount": "<?= esc($order['amount']) ?>",
+            "key": "<?php echo $key_id ?>",
+            "amount": <?php echo $order['amount'] ?>,
             "currency": "INR",
             "name": "AdventureShoppe",
             "description": "Transaction",
             "image": "<?php echo base_url() ?>public/logo.png",
-            "order_id": "<?= esc($order['id']) ?>",
+            "order_id": "<?php echo $order['id'] ?>",
+            "callback_url": "<?php echo base_url() ?>payment-status",
             "prefill": {
-                "name": "<?= esc($customerdata['name']) ?>",
-                "email": "<?= esc($customerdata['email']) ?>",
-                "contact": "<?= esc($customerdata['number']) ?>"
+                "name": "<?php echo $customerdata['name'] ?>",
+                "email": "<?php echo $customerdata['email'] ?>",
+                "contact": "<?php echo $customerdata['number'] ?>"
             },
             "notes": {
-                'user_id': "<?= esc($customerdata['user_id']) ?>",
-                'order_id': "<?= esc($order['id']) ?>",
-                'username': "<?= esc($customerdata['name']) ?>"
+                "address": "Razorpay Corporate Office",
+                'user_id': "<?php echo $customerdata['user_id'] ?>",
+                'order_id': "<?php echo $order['id'] ?>",
+                'username': "<?php echo $customerdata['name'] ?>"
             },
             "theme": {
                 "color": "#012652"
             },
             "modal": {
                 "ondismiss": function () {
-
-                    var cancelOrderID = "<?= esc($order['id']) ?>";
-
                     var error_data = {
                         reason: "User dismissed the payment modal",
-                        order_id: "<?= esc($cancel_orderid) ?>",
-                        razorpay_order_id: cancelOrderID,
-
+                        order_id: "<?php echo $order['id'] ?>"
                     };
                     var error_query = new URLSearchParams(error_data).toString();
-
-                    setTimeout(() => {
-                        window.location.href = "<?= base_url('payment-cancelled') ?>?" + error_query;
-                    }, 500);
+                    window.location.href = "<?php echo base_url() ?>payment-cancelled" + '/' + error_query;
                 }
             },
             "handler": function (response) {
+                var payment_data = {
+                    razorpay_payment_id: response.razorpay_payment_id,
+                    razorpay_order_id: response.razorpay_order_id,
+                    razorpay_signature: response.razorpay_signature,
+                    payment_method: response.method,
+                    order_id: "<?php echo $order['id'] ?>"
+                };
 
-                fbq('track', 'Purchase', {
-                    value: <?= esc($order['amount'] / 100) ?>,
-                    currency: 'INR'
-                });
+                // Send a POST request to your server
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "<?php echo base_url() ?>payment-status", true);
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+                xhr.onload = function () {
+                    if (xhr.status === 200) {
+                        try {
+                            var response = JSON.parse(xhr.responseText);
+
+                            if (response.code === 200 && response.status === 'success') {
+
+                                fbq('track', 'Purchase', {
+                                    value: <?= esc($order['amount'] / 100) ?>,
+                                    currency: 'INR'
+                                });
 
 
-                var loader = document.querySelector(".loader");
-                loader.style.display = "flex";
-                loader.style.position = "fixed";
-                loader.style.top = "0";
-                loader.style.left = "0";
-                setTimeout(function () {
-                    window.location.href = "<?= base_url('success') ?>"
-                }, 500);
+                                var loader = document.querySelector(".loader");
+                                loader.style.display = "flex";
+                                loader.style.position = "fixed";
+                                loader.style.top = "0";
+                                loader.style.left = "0";
+                                setTimeout(function () {
+                                    window.location.href = "<?= base_url('success') ?>"
+                                }, 500);
+
+                            } else {
+                                console.error("Error: ", response.message);
+                            }
+                        } catch (e) {
+                            console.error("Invalid JSON response:", xhr.responseText);
+                        }
+                    } else {
+
+                        console.error("HTTP Error: ", xhr.status, xhr.statusText);
+                    }
+                };
+
+
+                xhr.onerror = function () {
+                    console.error("Request failed. Status: ", xhr.status);
+                };
+
+                var data = new URLSearchParams(payment_data).toString();
+                xhr.send(data);
+
             }
         };
 
         var rzp1 = new Razorpay(options);
-
-        var InternalOrderId = "<?= esc($customerdata['order_id']) ?>";
-        var RazorpayOrderId = "<?= esc($order['id']) ?>";
-
 
         rzp1.on('payment.failed', function (response) {
             var error_data = {
@@ -98,26 +129,25 @@ require("components/head.php");
                 source: response.error.source,
                 step: response.error.step,
                 reason: response.error.reason,
-                order_id: InternalOrderId,
-                razorpay_order_id: RazorpayOrderId,
+                order_id: response.error.metadata.order_id,
                 payment_id: response.error.metadata.payment_id
             };
 
             var error_query = new URLSearchParams(error_data).toString();
-            setTimeout(() => {
-                window.location.href = "<?= base_url('payment-failed') ?>?" + error_query;
-            }, 500);
-
+            window.location.href = "<?php echo base_url() ?>payment-failed/" + error_query;
         });
 
-        window.onload = function () {
+        document.getElementById('rzp-button1').onclick = function (e) {
             rzp1.open();
             e.preventDefault();
         };
 
         document.getElementById('rzp-button1').click();
 
+
+
     </script>
+
 </body>
 
 </html>
